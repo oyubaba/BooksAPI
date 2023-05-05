fafc1c8781547e5fe9810dbab189326fcec84a55
Tatiane Vitoria Espindola Ribeiro <tatiane.ribeiro@strattner.com.br>
2023-05-05 11:38:32 -0300

    first commit

---
+++ source/repos/BooksAPI/BooksAPI/Repositories/IBookRepository.cs
@@ -0,0 +1,23 @@
+﻿using BooksAPI.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BooksAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+
+        Task<Book> Get(int Id);
+
+        Task<Book> Create(Book book);
+
+        Task Update(Book book);
+
+        Task Delete(int Id);
+
+
+
+    }
+}
